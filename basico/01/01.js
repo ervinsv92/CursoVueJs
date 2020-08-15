0cac4832cebea93987ad06e0793e0bbe75242c20
Ervin <ervinsv92@gmail.com>
2020-08-14 21:27:54 -0600

    01 inicio

---
+++ basico/01/01.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        titulo:'Hola mundo con Vue',
+        colores: ['Negro', 'Rojo', 'Blanco'],
+        frutas:[
+            {nombre:'Pera', cantidad:10},
+            {nombre:'Manzana', cantidad:0},
+            {nombre:'Platano', cantidad:11}
+        ],
+        nuevaFruta:'',
+        total:0
+    },
+    methods:{
+        agregarFruta (){
+            this.frutas.push({
+                nombre:this.nuevaFruta,
+                cantidad:0
+            });
+            this.nuevaFruta = '';
+        },
+        otroMetodo(){}
+    },
+    computed:{
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.frutas){
+                this.total += fruta.cantidad;
+            }
+            return this.total;
+        }
+    }
+});
