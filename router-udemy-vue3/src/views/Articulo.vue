3d4da2b262ae1e5be1a2ce851a79b8ce966c8438
Ervin Solano Vargas <esolanov@grupocolono.com>
2020-11-18 13:42:49 -0600

    fin route

---
+++ router-udemy-vue3/src/views/Articulo.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <Articulo texto="Ruta con parámetros" />
+    <h2>Parámetro: {{$route.params.id}}</h2>
+    <h3>Nombre: {{articulo.title}}</h3>
+    <p>{{articulo.id}} - {{articulo.body}}</p>
+</div>
+</template>
+
+<script>
+import Titulo from '../components/Titulo';
+export default {
+    components: {
+        Titulo
+    },
+    data() {
+        return {
+            articulo: {}
+        }
+    },
+    methods: {
+        async consumirArticulo() {
+            try {
+                const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`);
+                const array = await data.json();
+                this.articulo = array;
+            } catch (error) {
+
+            }
+        }
+    },
+    created(){
+        this.consumirArticulo();
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ router-udemy-vue3/src/views/Blog.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <Titulo texto="Titulo de mi Blog" />
+    <div v-for="item in arrayBlog" :key="item.id">
+    <router-link :to="`/blog/${item.id}`">
+        {{item.title}}
+    </router-link>
+        
+    </div>
+</div>
+</template>
+
+<script>
+import Titulo from "../components/Titulo"
+
+export default {
+    components: {
+        Titulo
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+        async consumirApi() {
+            try {
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+                const array = await data.json();
+                this.arrayBlog = array;
+            } catch (error) {
+
+            }
+        }
+    },
+    created(){
+        this.consumirApi();
+    }
+}
+</script>
+
+<style>
+
+</style>
