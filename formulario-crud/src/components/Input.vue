0f41edc94b96fb7aaa8b3d0099f89d05373adba6
Ervin <ervinsv92@gmail.com>
2020-11-28 12:41:25 -0600

    inicio video 89

---
+++ formulario-crud/src/components/Input.vue
@@ -0,0 +1,77 @@
+<template>
+  <input
+      type="text"
+      class="form-control mt-2"
+      placeholder="Ingrese nombre"
+      v-model.trim="tarea.nombre"
+    />
+
+    <div class="form-check form-check-inline">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        id="inlineCheckbox1"
+        value="javascript"
+        v-model="tarea.categorias"
+      />
+      <label class="form-check-label" for="inlineCheckbox1"> Javascript</label>
+    </div>
+
+    <div class="form-check form-check-inline">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        id="inlineCheckbox2"
+        value="nodejs"
+        v-model="tarea.categorias"
+      />
+      <label class="form-check-label" for="inlineCheckbox2"> Node.js</label>
+    </div>
+
+    <div class="mt-2">
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio1"
+          value="urgente"
+          v-model="tarea.estado"
+        />
+        <label class="form-check-label" for="inlineRadio1">Urgente</label>
+      </div>
+
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio2"
+          value="relax"
+          v-model="tarea.estado"
+        />
+        <label class="form-check-label" for="inlineRadio2">Relax</label>
+      </div>
+    </div>
+
+    <div class="mt-2">
+      <input type="number" v-model.number="tarea.numero" class="form-control">
+    </div>
+
+    <button class="btn btn-dark mt-2 btn-block" type="submit" :disabled="bloquear">
+      Procesar
+    </button>
+</template>
+
+<script>
+export default {
+    props:{
+        tarea: Object
+    },
+    computed:{
+        bloquear(){
+            return this.tarea.nombre == ""
+        }
+    }
+}
+</script>
---
+++ formulario-crud/src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent="procesarFormulario">
+    <Input :tarea="tarea"/>
+  </form>
+  <hr />
+  <p>{{ tarea }}</p>
+</template>
+
+<script>
+
+import Input from '../components/Input';
+import { mapActions } from 'vuex';
+const shortid = require('shortid')
+
+export default {
+  name: "Home",
+  components: {
+    Input
+  },
+  data() {
+    return {
+      tarea: {
+        id:"",
+        nombre: "",
+        categorias: [],
+        estado:"",
+        numero:0
+      },
+    };
+  },
+  methods:{
+    ...mapActions['setTareas'],
+    procesarFormulario(){
+      console.log(this.tarea)
+      if(this.tarea.nombre == ""){
+        console.log("Campo nombre vacio")
+        return
+      }
+      console.log("No está vacio")
+
+      this.tarea.id = shortid.generate()
+
+      this.setTareas(this.tarea)
+
+      this.tarea = {
+        id:"",
+        nombre: "",
+        categorias: [],
+        estado:"",
+        numero:0
+      }
+    }
+  },
+  
+};
+</script>
