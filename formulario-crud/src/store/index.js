5e610360dce54ea2de2641856d74f9e31bcc79a0
Ervin Solano Vargas <esolanov@grupocolono.com>
2020-12-01 08:57:17 -0600

    fin crud localstorage

--- formulario-crud/src/store/index.js
+++ formulario-crud/src/store/index.js
@@ -13,11 +13,16 @@ export default createStore({
     }
   },
   mutations: {
+    cargar(state, payload){
+      state.tareas = payload;
+    },
     set(state, payload){
       state.tareas.push(payload)
+      localStorage.setItem('tareas', JSON.stringify(state.tareas))
     },
     eliminar(state, payload){
       state.tareas = state.tareas.filter(x=>x.id != payload)
+      localStorage.setItem('tareas', JSON.stringify(state.tareas))
     },
     tarea(state, payload){
       if(!state.tareas.find(x => x.id === payload)){
@@ -28,10 +33,19 @@ export default createStore({
     },
     update(state, payload){
       state.tareas = state.tareas.map(item => item.id === payload.id ? payload:item)
+      localStorage.setItem('tareas', JSON.stringify(state.tareas))
       router.push('/')
     }
   },
   actions: {
+    cargarLocalStorage({commit}){
+      if(localStorage.getItem('tareas')){
+        commit('cargar', JSON.parse(localStorage.getItem('tareas')))
+        return
+      }
+
+      localStorage.setItem('tareas', JSON.stringify([]))
+    },
     setTareas({commit}, tarea){
       commit('set', tarea)
     },
